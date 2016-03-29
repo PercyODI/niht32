32c209707258e9d528e1a05ae3b13364c7979c67
percyodi <pearse.2008@gmail.com>
2016-03-29 20:24:56 +0000

    Changes to DashGum

--- codeigniter/application/views/activities/show.php
+++ codeigniter/application/views/activities/show.php
@@ -1,8 +1,26 @@
 <?php
 
 foreach($table as $row) {
-    print_r($row);
-    echo "<br><hr><br>";
+    ?>
+<div class="col-md-4 col-sm-4 mb">
+    <div class="darkblue-panel pn">
+        <div class="darkblue-header">
+            <h5><?= $row->name ?></h5>
+        </div>
+        <?= $row->id ?>
+        <?php 
+            for($i = 0; $i < 25; $i++) {
+                echo $row->name . "<br>";
+            }
+        ?>
+        <footer>
+            <div class="pull-left">
+                <h5><?= $row->date_time ?></h5>
+            </div>
+        </footer>
+    </div>
+</div>
+    <?php
 }
 
 ?>
